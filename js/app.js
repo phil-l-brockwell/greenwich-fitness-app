4e0c5ef3d7a07e1ee536bff7bbac0419dc2c42ca
Phil Brockwell <nineteen40eight@gmail.com>
2015-06-25 23:07:40 +0100

    added basic review page

--- js/app.js
+++ js/app.js
@@ -33,33 +33,26 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
 });
 
-app.factory('posts', function() {
-  var o = { posts: [{title: 'post 1', upvotes: 5},
-                    {title: 'post 2', upvotes: 2},
-                    {title: 'post 3', upvotes: 15},
-                    {title: 'post 4', upvotes: 9},
-                    {title: 'post 5', upvotes: 4}
+app.factory('reviews', function() {
+  var o = { reviews: [{text: 'review 1' },
+                      {text: 'review 2' },
+                      {text: 'review 3' },
+                      {text: 'review 4' },
+                      {text: 'review 5' }
                     ] };
   return o;
 });
 
-app.controller('MainCtrl', function($scope, posts) {
+app.controller('MainCtrl', function($scope, reviews) {
 
-  $scope.posts = posts.posts
+  $scope.reviews = reviews.reviews
 
-  $scope.addPost = function() {
-    if(!$scope.title || $scope.title === '') return;
-    $scope.posts.push({
-      title: $scope.title,
-      link: $scope.link,
-      upvotes: 0
+  $scope.addReview = function() {
+    if(!$scope.text || $scope.text === '') return;
+    $scope.reviews.push({
+      text: $scope.text
     });
-    $scope.title = '';
-    $scope.link = '';
-  };
-
-  $scope.incrementUpvotes = function(post) {
-    post.upvotes += 1;
+    $scope.text = '';
   };
 
 });
