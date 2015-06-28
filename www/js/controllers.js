ac737b3262debeb525eabd29c4177e1c2ed95034
Phil Brockwell <nineteen40eight@gmail.com>
2015-06-28 11:30:01 +0100

    refactored google map code and added location

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,62 +6,29 @@ angular.module('greenwichFitness.controllers', [])
   $scope.consultants = Consultants.consultants;
 })
 
-.controller('ContactCtrl', function($scope, $ionicLoading, $compile) {
+.controller('ContactCtrl', function($scope, $ionicLoading) {
 
   function initialize() {
-    var myLatlng = new google.maps.LatLng(43.07493,-89.381388);
+    var myLatlng = new google.maps.LatLng(51.477593,-0.010291);
 
     var mapOptions = {
       center: myLatlng,
       zoom: 16,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
+
     var map = new google.maps.Map(document.getElementById("map"),
         mapOptions);
-
-    //Marker + infowindow + angularjs compiled ng-click
-    var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
-    var compiled = $compile(contentString)($scope);
-
-    var infowindow = new google.maps.InfoWindow({
-      content: compiled[0]
-    });
 
     var marker = new google.maps.Marker({
       position: myLatlng,
       map: map,
-      title: 'Uluru (Ayers Rock)'
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-      infowindow.open(map,marker);
+      title: 'Greenwich Fitness'
     });
 
     $scope.map = map;
   }
   ionic.Platform.ready(initialize);
-
-  $scope.centerOnMe = function() {
-    if(!$scope.map) {
-      return;
-    }
-
-    $scope.loading = $ionicLoading.show({
-      content: 'Getting current location...',
-      showBackdrop: false
-    });
-
-    navigator.geolocation.getCurrentPosition(function(pos) {
-      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-      $ionicLoading.hide();
-    }, function(error) {
-      alert('Unable to get location: ' + error.message);
-    });
-  };
-
-  $scope.clickTest = function() {
-    alert('Example of infowindow with ng-click')
-  };
 })
 
 .controller('SettingsCtrl', function($scope) {
